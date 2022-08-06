c54b1995a490ba7a33fe47083f3c3ac2cc8b61cf
Rustam-Asdanov <asdanovrustam@gmail.com>
2022-08-06 17:03:06 +0400

    main page is created

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
+});
